eae7fd64aa094d4d43e105b20b12ec7c0e2b6bc6
Jeff.WK.Lin <Jeff.WK.Lin@Qisda.com>
2025-04-25 18:00:00 +0800

    Add project files.

---
+++ FileMergerTool/FileMerger.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace FileMergerTool
+{
+    partial class FileMerger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtFolderPath = new TextBox();
+            btnImportFiles = new Button();
+            btnOpen = new Button();
+            rtxIgnore = new RichTextBox();
+            btnMerge = new Button();
+            rtxFilesContent = new RichTextBox();
+            splMain = new SplitContainer();
+            splTop = new SplitContainer();
+            tlpnlExcludeSummary = new TableLayoutPanel();
+            gbxIgnore = new GroupBox();
+            gbxSummary = new GroupBox();
+            txtSummary = new TextBox();
+            gbxFilePath = new GroupBox();
+            clstFilePath = new CheckedListBox();
+            gbxFilesContent = new GroupBox();
+            btnClearSelect = new Button();
+            btnCopyAll = new Button();
+            ((System.ComponentModel.ISupportInitialize)splMain).BeginInit();
+            splMain.Panel1.SuspendLayout();
+            splMain.Panel2.SuspendLayout();
+            splMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splTop).BeginInit();
+            splTop.Panel1.SuspendLayout();
+            splTop.Panel2.SuspendLayout();
+            splTop.SuspendLayout();
+            tlpnlExcludeSummary.SuspendLayout();
+            gbxIgnore.SuspendLayout();
+            gbxSummary.SuspendLayout();
+            gbxFilePath.SuspendLayout();
+            gbxFilesContent.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtFolderPath
+            // 
+            txtFolderPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtFolderPath.Location = new Point(12, 12);
+            txtFolderPath.Name = "txtFolderPath";
+            txtFolderPath.Size = new Size(437, 23);
+            txtFolderPath.TabIndex = 0;
+            // 
+            // btnImportFiles
+            // 
+            btnImportFiles.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnImportFiles.Location = new Point(455, 61);
+            btnImportFiles.Name = "btnImportFiles";
+            btnImportFiles.Size = new Size(75, 55);
+            btnImportFiles.TabIndex = 1;
+            btnImportFiles.Text = "Import\r\nFiles";
+            btnImportFiles.UseVisualStyleBackColor = true;
+            btnImportFiles.Click += BtnImportFiles_Click;
+            // 
+            // btnOpen
+            // 
+            btnOpen.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnOpen.Location = new Point(455, 12);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new Size(75, 23);
+            btnOpen.TabIndex = 2;
+            btnOpen.Text = "Open";
+            btnOpen.UseVisualStyleBackColor = true;
+            btnOpen.Click += BtnOpen_Click;
+            // 
+            // rtxIgnore
+            // 
+            rtxIgnore.BorderStyle = BorderStyle.None;
+            rtxIgnore.Dock = DockStyle.Fill;
+            rtxIgnore.Location = new Point(3, 19);
+            rtxIgnore.Name = "rtxIgnore";
+            rtxIgnore.Size = new Size(191, 61);
+            rtxIgnore.TabIndex = 3;
+            rtxIgnore.Text = "";
+            // 
+            // btnMerge
+            // 
+            btnMerge.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnMerge.Location = new Point(455, 223);
+            btnMerge.Name = "btnMerge";
+            btnMerge.Size = new Size(75, 43);
+            btnMerge.TabIndex = 7;
+            btnMerge.Text = "Merge";
+            btnMerge.UseVisualStyleBackColor = true;
+            btnMerge.Click += BtnMerge_Click;
+            // 
+            // rtxFilesContent
+            // 
+            rtxFilesContent.BorderStyle = BorderStyle.None;
+            rtxFilesContent.Dock = DockStyle.Fill;
+            rtxFilesContent.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            rtxFilesContent.Location = new Point(3, 19);
+            rtxFilesContent.Name = "rtxFilesContent";
+            rtxFilesContent.Size = new Size(431, 180);
+            rtxFilesContent.TabIndex = 11;
+            rtxFilesContent.Text = "";
+            rtxFilesContent.WordWrap = false;
+            // 
+            // splMain
+            // 
+            splMain.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splMain.Location = new Point(12, 41);
+            splMain.Name = "splMain";
+            splMain.Orientation = Orientation.Horizontal;
+            // 
+            // splMain.Panel1
+            // 
+            splMain.Panel1.Controls.Add(splTop);
+            // 
+            // splMain.Panel2
+            // 
+            splMain.Panel2.Controls.Add(gbxFilesContent);
+            splMain.Size = new Size(437, 384);
+            splMain.SplitterDistance = 178;
+            splMain.TabIndex = 12;
+            // 
+            // splTop
+            // 
+            splTop.Dock = DockStyle.Fill;
+            splTop.Location = new Point(0, 0);
+            splTop.Name = "splTop";
+            // 
+            // splTop.Panel1
+            // 
+            splTop.Panel1.Controls.Add(tlpnlExcludeSummary);
+            // 
+            // splTop.Panel2
+            // 
+            splTop.Panel2.Controls.Add(gbxFilePath);
+            splTop.Size = new Size(437, 178);
+            splTop.SplitterDistance = 203;
+            splTop.TabIndex = 12;
+            // 
+            // tlpnlExcludeSummary
+            // 
+            tlpnlExcludeSummary.ColumnCount = 1;
+            tlpnlExcludeSummary.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlpnlExcludeSummary.Controls.Add(gbxIgnore, 0, 0);
+            tlpnlExcludeSummary.Controls.Add(gbxSummary, 0, 1);
+            tlpnlExcludeSummary.Dock = DockStyle.Fill;
+            tlpnlExcludeSummary.Location = new Point(0, 0);
+            tlpnlExcludeSummary.Name = "tlpnlExcludeSummary";
+            tlpnlExcludeSummary.RowCount = 2;
+            tlpnlExcludeSummary.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tlpnlExcludeSummary.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tlpnlExcludeSummary.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tlpnlExcludeSummary.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tlpnlExcludeSummary.Size = new Size(203, 178);
+            tlpnlExcludeSummary.TabIndex = 10;
+            // 
+            // gbxIgnore
+            // 
+            gbxIgnore.Controls.Add(rtxIgnore);
+            gbxIgnore.Dock = DockStyle.Fill;
+            gbxIgnore.Location = new Point(3, 3);
+            gbxIgnore.Name = "gbxIgnore";
+            gbxIgnore.Size = new Size(197, 83);
+            gbxIgnore.TabIndex = 13;
+            gbxIgnore.TabStop = false;
+            gbxIgnore.Text = "Ignore";
+            // 
+            // gbxSummary
+            // 
+            gbxSummary.Controls.Add(txtSummary);
+            gbxSummary.Dock = DockStyle.Fill;
+            gbxSummary.Location = new Point(3, 92);
+            gbxSummary.Name = "gbxSummary";
+            gbxSummary.Size = new Size(197, 83);
+            gbxSummary.TabIndex = 14;
+            gbxSummary.TabStop = false;
+            gbxSummary.Text = "Summary";
+            // 
+            // txtSummary
+            // 
+            txtSummary.Dock = DockStyle.Fill;
+            txtSummary.Location = new Point(3, 19);
+            txtSummary.Multiline = true;
+            txtSummary.Name = "txtSummary";
+            txtSummary.Size = new Size(191, 61);
+            txtSummary.TabIndex = 9;
+            // 
+            // gbxFilePath
+            // 
+            gbxFilePath.Controls.Add(clstFilePath);
+            gbxFilePath.Dock = DockStyle.Fill;
+            gbxFilePath.Location = new Point(0, 0);
+            gbxFilePath.Name = "gbxFilePath";
+            gbxFilePath.Size = new Size(230, 178);
+            gbxFilePath.TabIndex = 13;
+            gbxFilePath.TabStop = false;
+            gbxFilePath.Text = "FilePath";
+            // 
+            // clstFilePath
+            // 
+            clstFilePath.CheckOnClick = true;
+            clstFilePath.Dock = DockStyle.Fill;
+            clstFilePath.FormattingEnabled = true;
+            clstFilePath.Location = new Point(3, 19);
+            clstFilePath.Name = "clstFilePath";
+            clstFilePath.Size = new Size(224, 156);
+            clstFilePath.TabIndex = 13;
+            clstFilePath.ItemCheck += ClstFilePath_ItemCheck;
+            // 
+            // gbxFilesContent
+            // 
+            gbxFilesContent.Controls.Add(rtxFilesContent);
+            gbxFilesContent.Dock = DockStyle.Fill;
+            gbxFilesContent.Location = new Point(0, 0);
+            gbxFilesContent.Name = "gbxFilesContent";
+            gbxFilesContent.Size = new Size(437, 202);
+            gbxFilesContent.TabIndex = 13;
+            gbxFilesContent.TabStop = false;
+            gbxFilesContent.Text = "FilesContent";
+            // 
+            // btnClearSelect
+            // 
+            btnClearSelect.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnClearSelect.Location = new Point(455, 122);
+            btnClearSelect.Name = "btnClearSelect";
+            btnClearSelect.Size = new Size(75, 55);
+            btnClearSelect.TabIndex = 13;
+            btnClearSelect.Text = "Clear\r\nSelect";
+            btnClearSelect.UseVisualStyleBackColor = true;
+            btnClearSelect.Click += BtnClearSelect_Click;
+            // 
+            // btnCopyAll
+            // 
+            btnCopyAll.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnCopyAll.Location = new Point(455, 272);
+            btnCopyAll.Name = "btnCopyAll";
+            btnCopyAll.Size = new Size(75, 43);
+            btnCopyAll.TabIndex = 14;
+            btnCopyAll.Text = "CopyAll";
+            btnCopyAll.UseVisualStyleBackColor = true;
+            btnCopyAll.Click += BtnCopyAll_Click;
+            // 
+            // FileMerger
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(542, 437);
+            Controls.Add(btnCopyAll);
+            Controls.Add(btnClearSelect);
+            Controls.Add(splMain);
+            Controls.Add(btnMerge);
+            Controls.Add(btnOpen);
+            Controls.Add(btnImportFiles);
+            Controls.Add(txtFolderPath);
+            Name = "FileMerger";
+            Text = "FileMerger";
+            FormClosing += ImportDialog_FormClosing;
+            splMain.Panel1.ResumeLayout(false);
+            splMain.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splMain).EndInit();
+            splMain.ResumeLayout(false);
+            splTop.Panel1.ResumeLayout(false);
+            splTop.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splTop).EndInit();
+            splTop.ResumeLayout(false);
+            tlpnlExcludeSummary.ResumeLayout(false);
+            gbxIgnore.ResumeLayout(false);
+            gbxSummary.ResumeLayout(false);
+            gbxSummary.PerformLayout();
+            gbxFilePath.ResumeLayout(false);
+            gbxFilesContent.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtFolderPath;
+        private Button btnImportFiles;
+        private Button btnOpen;
+        private RichTextBox rtxIgnore;
+        private RichTextBox txtPathTree;
+        private Button btnMerge;
+        private RichTextBox rtxFilesContent;
+        private SplitContainer splMain;
+        private TextBox txtSummary;
+        private SplitContainer splTop;
+        private TableLayoutPanel tlpnlExcludeSummary;
+        private GroupBox gbxIgnore;
+        private GroupBox gbxSummary;
+        private GroupBox gbxFilePath;
+        private GroupBox gbxFilesContent;
+        private CheckedListBox clstFilePath;
+        private Button btnClearSelect;
+        private Button btnCopyAll;
+    }
+}
---
+++ FileMergerTool/FileMerger.cs
@@ -0,0 +1,329 @@
+﻿using FileMergerTool.Properties;
+using System.Data;
+using System.Text;
+
+namespace FileMergerTool
+{
+    public partial class FileMerger : Form
+    {
+        #region FIELDS
+
+        private readonly List<FileInfo> fileList = [];
+        private List<string> excludeList = [];
+        private static readonly string[] separator = ["\r\n", "\r", "\n"];
+        private const int SIZE_LIMIT_MB = 10; // Maximum total file size limit in MB (10 MB)
+        private const int CHECK_INTERVAL = 25; // Check file size every 25 files
+
+        #endregion FIELDS
+
+        #region CONSTRUCTOR
+
+        /// <summary>
+        /// Initializes a new instance of the FileMergerTool form.
+        /// </summary>
+        public FileMerger()
+        {
+            InitializeComponent();
+            LoadSettings();
+        }
+
+        #endregion CONSTRUCTOR
+
+        #region FORM_EVENT_HANDLERS
+
+        /// <summary>
+        /// Saves settings when the form is closing.
+        /// </summary>
+        private void ImportDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.FolderPath = txtFolderPath.Text;
+            Settings.Default.Ignore = rtxIgnore.Text;
+            Settings.Default.Save();
+        }
+
+        #endregion FORM_EVENT_HANDLERS
+
+        #region BUTTON_EVENT_HANDLERS
+
+        /// <summary>
+        /// Opens a folder browser dialog to select a folder and updates the folder path text box.
+        /// </summary>
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            using var folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFolderPath.Text = folderDialog.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// Initiates the file scanning process.
+        /// </summary>
+        private void BtnImportFiles_Click(object sender, EventArgs e)
+        {
+            ScanFiles();
+        }
+
+        /// <summary>
+        /// Clears the selected files in the checklist and updates the summary.
+        /// </summary>
+        private void BtnClearSelect_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clstFilePath.Items.Count; i++)
+            {
+                clstFilePath.SetItemChecked(i, false);
+            }
+            fileList.Clear();
+            UpdateSummary();
+        }
+
+        /// <summary>
+        /// Merges the content of selected files and displays it in the content text box.
+        /// </summary>
+        private void BtnMerge_Click(object sender, EventArgs e)
+        {
+            var sb = new StringBuilder();
+            fileList.ForEach(f =>
+            {
+                sb.AppendLine($"================================================");
+                sb.AppendLine($"FILE: {f.FullName}");
+                sb.AppendLine($"================================================");
+                sb.AppendLine(File.ReadAllText(f.FullName));
+                sb.AppendLine($"================================================");
+            });
+
+            rtxFilesContent.Text = sb.ToString();
+        }
+
+        /// <summary>
+        /// Copies the merged file content to the clipboard and shows a summary of the copied text.
+        /// </summary>
+        private void BtnCopyAll_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(rtxFilesContent.Text))
+                {
+                    MessageBox.Show("沒有內容可複製！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                Clipboard.SetText(rtxFilesContent.Text);
+
+                int charCount = rtxFilesContent.Text.Length;
+                int lineCount = rtxFilesContent.Text.Split(separator, StringSplitOptions.None).Length;
+
+                MessageBox.Show(
+                    $"已成功複製進剪貼簿\r\n" +
+                    $"字數: {charCount} \r\n" +
+                    $"行數: {lineCount} \r\n",
+                    "複製成功",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"複製失敗：{ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Updates the file list and summary when a file's check state changes in the checklist.
+        /// </summary>
+        private void ClstFilePath_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            BeginInvoke(new Action(() =>
+            {
+                fileList.Clear();
+                for (int i = 0; i < clstFilePath.Items.Count; i++)
+                {
+                    if (clstFilePath.GetItemChecked(i) || (i == e.Index && e.NewValue == CheckState.Checked))
+                    {
+                        string path = clstFilePath.Items[i]?.ToString() ?? "empty";
+                        fileList.Add(new FileInfo(path));
+                    }
+                }
+                UpdateSummary();
+            }));
+        }
+
+        #endregion BUTTON_EVENT_HANDLERS
+
+        #region CORE_FUNCTIONALITY
+
+        /// <summary>
+        /// Loads default or saved settings into the form controls.
+        /// </summary>
+        private void LoadSettings()
+        {
+            txtFolderPath.Text = Settings.Default.FolderPath;
+            if (string.IsNullOrEmpty(txtFolderPath.Text))
+            {
+                txtFolderPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            rtxIgnore.Text = Settings.Default.Ignore;
+            if (string.IsNullOrEmpty(rtxIgnore.Text))
+            {
+                rtxIgnore.Text = @"bin/
+obj/
+.git
+.vs
+.user
+.csproj
+.config
+.xml
+.resx
+.dll
+.pdb
+.ini
+.exe
+.xlsx";
+            }
+        }
+
+        /// <summary>
+        /// Scans the selected directory for files, applies exclusion filters, and updates the UI.
+        /// </summary>
+        private void ScanFiles()
+        {
+            fileList.Clear();
+            excludeList = [.. rtxIgnore.Text.Split(separator, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim())];
+
+            if (string.IsNullOrEmpty(txtFolderPath.Text) || !Directory.Exists(txtFolderPath.Text))
+            {
+                MessageBox.Show("請選擇有效的資料夾路徑！", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            ScanDirectory(new DirectoryInfo(txtFolderPath.Text));
+            GenerateTreeViewOutput();
+            UpdateSummary();
+        }
+
+        /// <summary>
+        /// Recursively scans a directory and its subdirectories for files, respecting exclusion rules.
+        /// </summary>
+        /// <param name="dir">The directory to scan.</param>
+        private void ScanDirectory(DirectoryInfo dir)
+        {
+            try
+            {
+                if (IsExcludedFolder(dir.FullName))
+                    return;
+
+                int fileCount = 0;
+
+                foreach (var file in dir.GetFiles())
+                {
+                    if (!IsExcludedFile(file))
+                    {
+                        fileList.Add(file);
+                        fileCount++;
+
+                        if (fileCount % CHECK_INTERVAL == 0)
+                        {
+                            long totalSizeMB = fileList.Sum(f => f.Length) / 1024 / 1024;
+                            if (totalSizeMB > SIZE_LIMIT_MB)
+                            {
+                                var result = MessageBox.Show(
+                                    $"目前檔案總大小已超過 {SIZE_LIMIT_MB} MB ({totalSizeMB} MB)。是否繼續掃描？",
+                                    "檔案大小限制",
+                                    MessageBoxButtons.YesNo,
+                                    MessageBoxIcon.Question);
+
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                foreach (var subDir in dir.GetDirectories())
+                {
+                    ScanDirectory(subDir);
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                MessageBox.Show(ex.Message, "掃描已停止", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"掃描時發生錯誤：{ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Determines if a folder should be excluded based on the ignore list.
+        /// </summary>
+        /// <param name="folderPath">The full path of the folder to check.</param>
+        /// <returns>True if the folder is excluded; otherwise, false.</returns>
+        private bool IsExcludedFolder(string folderPath)
+        {
+            string relativePath = Path.GetRelativePath(txtFolderPath.Text, folderPath);
+            if (relativePath == ".")
+                return false;
+
+            relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+
+            return excludeList.Any(exclude =>
+            {
+                string excludeFolder = exclude.TrimEnd('/').Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
+                return relativePath.Contains(Path.DirectorySeparatorChar + excludeFolder + Path.DirectorySeparatorChar) ||
+                       relativePath.EndsWith(Path.DirectorySeparatorChar + excludeFolder) ||
+                       relativePath == excludeFolder;
+            });
+        }
+
+        /// <summary>
+        /// Determines if a file should be excluded based on its extension or name.
+        /// </summary>
+        /// <param name="file">The file to check.</param>
+        /// <returns>True if the file is excluded; otherwise, false.</returns>
+        private bool IsExcludedFile(FileInfo file)
+        {
+            return excludeList.Any(exclude =>
+                file.Extension.Equals(exclude, StringComparison.OrdinalIgnoreCase) ||
+                file.Name.EndsWith(exclude, StringComparison.OrdinalIgnoreCase));
+        }
+
+        /// <summary>
+        /// Populates the checklist with scanned files, defaulting to all checked.
+        /// </summary>
+        private void GenerateTreeViewOutput()
+        {
+            clstFilePath.Items.Clear();
+            foreach (var file in fileList)
+            {
+                clstFilePath.Items.Add(file.FullName, true);
+            }
+        }
+
+        /// <summary>
+        /// Updates the summary text box with file count and total size.
+        /// </summary>
+        private void UpdateSummary()
+        {
+            var sb2 = new StringBuilder();
+            long contentSize = 0;
+            fileList.ForEach(f =>
+            {
+                sb2.AppendLine($"================================================");
+                sb2.AppendLine($"FILE: {f.FullName}");
+                sb2.AppendLine($"================================================");
+                sb2.AppendLine($"================================================");
+                contentSize += f.Length;
+            });
+
+            var sb = new StringBuilder();
+            sb.AppendLine($"File Count : {fileList.Count}");
+            sb.AppendLine($"File Size : {(contentSize + sb2.Capacity) / 1024}k");
+
+            txtSummary.Text = sb.ToString();
+        }
+
+        #endregion CORE_FUNCTIONALITY
+    }
+}
